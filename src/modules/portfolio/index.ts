44a1aec73700879718c98628c0cce31e3cbfc498
Levin Bänninger <levin@baenninger.me>
2026-07-15 14:00:08 +0200

    refactor(portfolio): flatten destinations

--- src/modules/portfolio/index.ts
+++ src/modules/portfolio/index.ts
@@ -1,4 +1,4 @@
 export { PORTFOLIO_NAME } from "./content";
 export { SOCIAL_PROFILES } from "./about/social/profiles";
-export { PORTFOLIO_DESTINATIONS } from "./navigation/destinations";
+export { PORTFOLIO_DESTINATIONS } from "./destinations";
 export { PortfolioView } from "./portfolio-view";
